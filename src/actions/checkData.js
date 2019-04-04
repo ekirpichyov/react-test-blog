63d55576e3805909a1a40a8a3bbe9dab234fd05c
ekirpichyov <47474371+ekirpichyov@users.noreply.github.com>
2019-04-04 11:04:02 +0300

    fix broke with invalid data

--- src/actions/checkData.js
+++ src/actions/checkData.js
@@ -2,14 +2,33 @@ import json from "../data/data.json"
 
 //проверка наличия данных в localStorage
 
+const validData = (element) => {
+    return (
+    typeof element === "object" &&
+    "key" in element && 
+    "data" in element &&
+    "head" in element.data &&
+    "comments" in element.data &&
+    "brief" in element.data &&
+    "content" in element.data
+    )
+}
+
 const checkData = () => {
+
     if (localStorage.length === 0) {
         json.map(element => localStorage.setItem(element.key, JSON.stringify(element)))
     }
         
     const localData = []
+    let element
     for(let i = 0; i < localStorage.length; i++) {
-        localData[i] = JSON.parse(localStorage.getItem(localStorage.key(i)))
+        element = JSON.parse(localStorage.getItem(localStorage.key(i)))
+        if (validData(element)) {
+            localData[i] = element
+        } else {
+            continue
+        }
     }
     return localData.sort((a,b)=>{
         return a.key < b.key ? 1 :
