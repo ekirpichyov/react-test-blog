42ef96543b4830f996c00b51ca5124e8167ca83b
ekirpichyov <47474371+ekirpichyov@users.noreply.github.com>
2019-04-04 09:50:21 +0300

    firefox fixes

--- src/actions/checkData.js
+++ src/actions/checkData.js
@@ -11,7 +11,9 @@ const checkData = () => {
     for(let i = 0; i < localStorage.length; i++) {
         localData[i] = JSON.parse(localStorage.getItem(localStorage.key(i)))
     }
-    return localData.reverse()
+    return localData.sort((a,b)=>{
+        return a.key < b.key ? 1 :
+        a.key > b.key ? -1 : 0})
 }
 
 export default checkData
