d95e30784509ae6f010be4c31878f3c5be7922b4
ekirpichyov <47474371+ekirpichyov@users.noreply.github.com>
2019-04-04 09:42:42 +0300

    Updates

--- actions/checkData.js
+++ actions/checkData.js
@@ -9,7 +9,9 @@ const checkData = () => {
     for(let i = 0; i < localStorage.length; i++) {
         localData[i] = JSON.parse(localStorage.getItem(localStorage.key(i)))
     }
-    return localData.reverse()
+    return localData.sort((a,b)=>{
+        return a.key > b.key ? 1 :
+        a.key < b.key ? -1 : 0})
 }
 
 export default checkData
